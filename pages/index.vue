54a1b5ee285f9962367b3037d8b83870c6dbf936
CareTiger <currency@gmail.com>
2022-08-30 19:35:59 -0500

    updates

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <ul role="list" class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 container mx-auto">
-        <li class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-gray-100 text-center shadow hover:cursor-pointer" v-for="photo in data" :key="photo.id">
+    <ul role="list" class="grid grid-cols-1 gap-6 container mx-auto" ref="scrollComponent">
+        <li class="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-gray-100 text-center shadow hover:cursor-pointer" v-for="photo in photos" :key="photo.id">
             <!-- <nuxt-link :to="`/photos/:${photo.url}`" @click.native="gotoDetails(photo.url)"> -->
             
             <nuxt-link to="" @click.native="gotoDetails(photo.url)">
@@ -15,13 +15,51 @@
 
 <script setup>
 const router = useRouter();
+const scrollComponent = ref(null)
 
+// Fetch the data 
 const data = await $fetch('https://jsonplaceholder.typicode.com/photos', {
                     method: 'GET',
                 })
-console.log("Nos of photos " + data.length)  
+console.log("total Nos of photos fetched " + data.length)  
 
-async function gotoDetails(url) {
+const getPhotos = (number) => {
+    let ret = []
+    for (let index = 0; index < number; index++) {
+        ret.push(data[index]);    
+    }
+    return ret
+}
+
+const photos = ref(getPhotos(10))
+
+//  creating a method that loads in 10 posts at a time and appends them to our posts variable.
+const loadMorePhotos = () => {
+        let newPhotos = getPhotos(10)
+        console.log(newPhotos)
+        photos.value.push(...newPhotos)
+    }
+
+// trigger loadMorePosts method. by adding an event listener that listens to a scroll event and calls a method. We are going to add it when the component is mounted and remove it when the component is unmounted (destroyed).  
+onMounted(() => {
+    window.addEventListener("scroll", handleScroll)
+})
+
+onUnmounted(() => {
+    window.removeEventListener("scroll", handleScroll)
+})
+
+const handleScroll = (e) => {
+    let element = scrollComponent.value
+    if (element.getBoundingClientRect().bottom < window.innerHeight) {
+        loadMorePhotos()
+    }
+}
+
+
+
+// dynamic routing to show the image detail
+/* async function gotoDetails(url) {
     console.log(url)
     try {
         await navigateTo({
@@ -33,5 +71,5 @@ async function gotoDetails(url) {
         console.log(error)
     }
     return
-}              
+}       */        
 </script>
